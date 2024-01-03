993bcca132763d9f9f36607033631db2bb36e803
Kush Jayesh Ahir <ahir.kush1649@gmail.com>
2024-01-03 10:47:11 -0700

    Add files via upload

---
+++ Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Magic 8 Ball
+//
+//  Created by Angela Yu on 14/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var ball_image: UIImageView!
+    
+
+    @IBAction func ask_button(_ sender: UIButton) {
+        
+        let ballimage = [UIImage(named: "ball1"), UIImage(named: "ball2") , UIImage(named: "ball3"), UIImage(named: "ball4"), UIImage(named: "ball5")]
+        
+        ball_image.image = ballimage[Int.random(in: 0 ..< 5)]
+    }
+}
+
